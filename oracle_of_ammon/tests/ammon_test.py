881b34902f38fcbf0163cc6b94c340111efacc9e
Kyle McLester <kmcleste@uncc.edu>
2023-01-21 23:10:57 -0500

    Update pytests

--- oracle_of_ammon/tests/ammon_test.py
+++ oracle_of_ammon/tests/ammon_test.py
@@ -3,6 +3,7 @@ import pathlib
 
 from fastapi.testclient import TestClient
 from pydantic import parse_obj_as
+from httpx import Response
 
 from oracle_of_ammon.api.ammon import app
 from oracle_of_ammon.api.models import (
@@ -17,45 +18,69 @@ client = TestClient(app)
 
 
 def test_root():
-    response = client.get("/")
+    response: Response = client.get("/")
     assert response.status_code == 200
 
 
 def test_health():
-    response = client.get("/health")
+    response: Response = client.get("/health")
     assert response.status_code == 200
     assert parse_obj_as(HealthResponse, response.json())
 
 
 def test_get_documents():
-    response = client.get("/get-documents")
+    response: Response = client.post(
+        "/get-documents", json={"index": "document", "is_faq": True}
+    )
     assert response.status_code == 200
     assert parse_obj_as(Documents, response.json())
 
 
 def test_summary_empty_docstore():
-    response = client.get("/summary")
+    response: Response = client.post(
+        "/summary", json={"index": "document", "is_faq": True}
+    )
     assert response.status_code == 404
     assert parse_obj_as(HTTPError, response.json())
 
 
-def test_upload():
+def test_faq_upload():
     file = open(
         file=pathlib.Path(os.getcwd(), "oracle_of_ammon", "data", "faq.csv"), mode="br"
     )
-    response = client.post("/upload-documents", files={"files": file})
+    response: Response = client.post(
+        "/upload-documents",
+        files={"files": file},
+        json={"index": "document", "is_faq": True},
+    )
+    assert response.status_code == 201
+    file.close()
+
+
+def test_semantic_upload():
+    file = open(
+        file=pathlib.Path(os.getcwd(), "oracle_of_ammon", "data", "semantic.txt"),
+        mode="br",
+    )
+    response: Response = client.post(
+        "/upload-documents",
+        files={"files": file},
+        json={"index": "document", "is_faq": False},
+    )
     assert response.status_code == 201
     file.close()
 
 
 def test_summary():
-    response = client.get("/summary")
+    response: Response = client.post(
+        "/summary", json={"index": "document", "is_faq": False}
+    )
     assert response.status_code == 200
     assert parse_obj_as(Summary, response.json())
 
 
 def test_faq_search():
-    response = client.post(
+    response: Response = client.post(
         "/faq-search",
         json={
             "query": "Why are duplicate answers being returned?",
@@ -66,12 +91,24 @@ def test_faq_search():
     assert parse_obj_as(SearchResponse, response.json())
 
 
+def test_extractive_search():
+    response: Response = client.post(
+        "/extractive-search",
+        json={
+            "query": "How for is Siwa from Memphis?",
+            "params": {"Retriever": {"top_k": 3}, "Reader": {"top_k": 3}},
+        },
+    )
+    assert response.status_code == 200
+    assert parse_obj_as(SearchResponse, response.json())
+
+
 def test_document_search():
-    response = client.post(
+    response: Response = client.post(
         "/document-search",
         json={
-            "query": "Why are duplicate answers being returned?",
-            "params": {"Retriever": {"top_k": 10}},
+            "query": "Location of Ammon",
+            "params": {"Retriever": {"top_k": 3}},
         },
     )
     assert response.status_code == 200
--- oracle_of_ammon/tests/oracle_test.py
+++ oracle_of_ammon/tests/oracle_test.py
@@ -7,21 +7,36 @@ oracle = Oracle()
 
 
 def test_oracle_init():
-    assert hasattr(oracle, "document_store")
-    assert hasattr(oracle, "retriever")
+    assert hasattr(oracle, "faq_document_store")
+    assert hasattr(oracle, "semantic_document_store")
+    assert hasattr(oracle, "faq_retriever")
+    assert hasattr(oracle, "semantic_retriever")
     assert hasattr(oracle, "faq_pipeline")
+    assert hasattr(oracle, "extractive_pipeline")
     assert hasattr(oracle, "document_search_pipeline")
 
 
-def test_index_documents():
+def test_index_faq():
     path = pathlib.Path(os.getcwd(), "oracle_of_ammon", "data", "faq.csv").as_posix()
-    oracle.index_documents(filepath_or_buffer=path)
-    assert oracle.document_store.get_document_count() != 0
+    oracle.index_documents(filepath_or_buffer=path, **{"is_faq": True})
+    assert oracle.faq_document_store.get_document_count() != 0
+
+
+def test_index_documents():
+    path = pathlib.Path(
+        os.getcwd(), "oracle_of_ammon", "data", "semantic.txt"
+    ).as_posix()
+    oracle.index_documents(filepath_or_buffer=path, **{"is_faq": False})
+    assert oracle.semantic_document_store.get_document_count() != 0
 
 
 def test_faq_search():
     assert oracle.faq_search(query="Why are duplicate questions being returned?")
 
 
+def test_extractive_search():
+    assert oracle.extractive_search(query="How far is Siwa from Memphis?")
+
+
 def test_document_search():
-    assert oracle.document_search(query="Why are duplicate questions being returned?")
+    assert oracle.document_search(query="Climate of Ammon")
