c9a29d8c787be04149f394ac47feb626c0d8a098
Kyle McLester <kmcleste@uncc.edu>
2023-01-21 23:49:24 -0500

    Update locust

--- oracle_of_ammon/locust/locust.py
+++ oracle_of_ammon/locust/locust.py
@@ -1,27 +1,14 @@
 from locust import HttpUser, task, between
 
 
+# TODO: Dynamically set tasks based on config file
 class Tests(HttpUser):
     wait_time = between(2, 4)
-
-    @task
-    def search(self):
-        self.client.post(
-            url="/search",
-            json={
-                "query": "This is a question",
-                "params": {"Retriever": {"top_k": 10}},
-            },
-        )
 
     @task
     def health(self):
         self.client.get(url="/health")
 
     @task
-    def get_documents(self):
-        self.client.get(url="/get-documents")
-
-    @task
-    def summary(self):
-        self.client.get(url="/summary")
+    def root(self):
+        self.client.get(url="/")
